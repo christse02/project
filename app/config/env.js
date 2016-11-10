650f5abd1592d797295a1b6a73ac1cce4a060fdd
christse02 <christse02@gmail.com>
2016-11-10 23:32:43 +0800

    env

--- app/config/env.js
+++ app/config/env.js
@@ -1,6 +1,15 @@
 env = {
-  FB_API_KEY: 'AIzaSyDTXQFSuriwxpvJd0mZHElmLmhL8AIYmWE',
-  FB_AUTH_DOMAIN: 'teamform-15bcb.firebaseapp.com',
-  FB_DATABASE_URL: 'https://teamform-15bcb.firebaseio.com',
-  FB_STORAGE_BUCKET: 'teamform-15bcb.appspot.com'
+  FB_API_KEY: 'AIzaSyDULfYNZxcZgu5OYtI3aOjDuVLU_8ngYvU',
+  FB_AUTH_DOMAIN: 'comp3111project-88170.firebaseapp.com',
+  FB_DATABASE_URL: 'https://comp3111project-88170.firebaseio.com',
+  FB_STORAGE_BUCKET: 'comp3111project-88170.appspot.com'
+
+  /*
+    apiKey: "AIzaSyDULfYNZxcZgu5OYtI3aOjDuVLU_8ngYvU",
+    authDomain: "comp3111project-88170.firebaseapp.com",
+    databaseURL: "https://comp3111project-88170.firebaseio.com",
+    storageBucket: "comp3111project-88170.appspot.com"
+
+  */
 }
+ //firebase.initializeApp(config);
